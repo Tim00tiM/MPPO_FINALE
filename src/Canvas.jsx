5ce9d6be526a56e1dfe8974ce1083ce46691be75
tim00tim <olimprofl@gmail.com>
2022-04-17 15:54:53 +0300

    df

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -4,14 +4,25 @@ const Canvas = props => {
 
     const canvasRef = useRef(null)
 
+    const draw = ctx => {
+        props.map(item => {
+        ctx.fillstyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(item.x, item.y, item.rad, 0, 2*Math.PI)
+        ctx.stroke()
+        }
+        )}
+
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        //Our first draw
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-      }, [])
-return <canvas ref = {canvasRef} {...props}/>
+
+        draw(context)
+        //Our first draw 
+      }, [canvasRef])
+
+
+return <canvas ref = {canvasRef}/>
 
 }
 
